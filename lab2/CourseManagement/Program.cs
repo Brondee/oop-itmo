f8891f273d669ef13a248ed3267503731c1a80e2
Артем Колпаков <artemkolpakov@MacBook-Air-Artem-6.local>
2025-11-23 15:45:19 +0300

    changed program.cs

--- lab2/CourseManagement/Program.cs
+++ lab2/CourseManagement/Program.cs
@@ -12,7 +12,7 @@ public class Program
     var teacher = new Teacher(1, "Иванов Иван");
     var student = new Student(1, "Петров Петр");
 
-    var onlineCourse = new OnlineCourse(1, "Основы C#", "Moodle", "https://university.example.com/csharp");
+    var onlineCourse = new OnlineCourse(1, "Основы C#", "Backboost", "https://backboost.courses.itmo.ru/csharp");
     onlineCourse.AssignTeacher(teacher);
     onlineCourse.EnrollStudent(student);
 
